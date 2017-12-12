92e772d61a1dae7005f2160d2c2228c50b798f44
julia sheremet <juliajessicasher@gmail.com>
2017-12-11 21:12:20 -0800

    add address form to html and js files

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,15 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
 }
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
 Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 }
@@ -17,6 +25,8 @@ $(document).ready(function() {
     var inputtedLastName = $("input#new-last-name").val();
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var lisa = new Contact("Lisa", "Simpson");
+    console.log(lisa);
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
@@ -30,5 +40,12 @@ $(document).ready(function() {
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+  });
+});
+
+$(document).ready(function() {
+  $("#add-address").click(function() { //calling to the button
+    $("#new-addresses").append('<div class="new-addresse">' + '<div class=form-group>' + '<label for="new-street">Street</label>' + '<input type="text" class="form-control new-street">' + '</div>' + '<div class="form-group">' +'<label for="new-city">City</label>' + '<input type="text" class="form-control new-city">' + '</div>' + '<div class="form-group">' + '<label for="new-state">State</label>' + '<input type="text" class="form-control new-state">' + '</div>' + '</div>');
+
   });
 });
