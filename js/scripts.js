7a955d3e3ec5895723662ce18d4b720dfa531edc
julia sheremet <juliajessicasher@gmail.com>
2017-12-11 21:51:30 -0800

    final comit for today

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//business logic
+////////////////////-----------------------------------------//business logic
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
@@ -14,11 +14,31 @@ function Address(street, city, state) {
 Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 }
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
 
-// user interface logic
-$(document).ready(function() { //first function
-  $("form#new-contact").submit(function(event) {
-    debugger;
+//////////////////////////////////-------------------------// user interface logic
+$(document).ready(function() { //function for grouping the address
+  $("#add-address").click(function() { //calling to the button
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+
+});
+  $("form#new-contact").submit(function(event) { //original first function
+    //debugger;
     event.preventDefault();
 
     var inputtedFirstName = $("input#new-first-name").val(); //variables created for New Contact to concat the results entered in seperate inputs
@@ -31,27 +51,24 @@ $(document).ready(function() { //first function
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
       var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState); //new address object
-      newContact.addresses.push(newAddress);      
+      newContact.addresses.push(newAddress);
     });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>"); //sending list of contact name to html document
-
+    //debugger;
 
     $(".contact").last().click(function() { //function for contact name and display
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+      $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
     });
 
     $("input#new-first-name").val(""); //taking in the input if the name
     $("input#new-last-name").val("");
-  });
-});
-
-$(document).ready(function() { //function for grouping the address
-  $("#add-address").click(function() { //calling to the button
-    $("#new-addresses").append('<div class="new-addresse">' + '<div class=form-group>' + '<label for="new-street">Street</label>' + '<input type="text" class="form-control new-street">' + '</div>' + '<div class="form-group">' +'<label for="new-city">City</label>' + '<input type="text" class="form-control new-city">' + '</div>' + '<div class="form-group">' + '<label for="new-state">State</label>' + '<input type="text" class="form-control new-state">' + '</div>' + '</div>');
-
   });
 });
